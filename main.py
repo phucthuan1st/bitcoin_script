98b15a7ce925d3c734151b84ce794afcb0d049de
phucthuan1st <phucthuan1st@gmail.com>
2023-12-21 20:38:08 +0700

    change create mode to wallet mode

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ import sys
 def main():
     if len(sys.argv) < 2 or sys.argv[1] in ['help', '--help', None]:
         print("Usage:")
-        print("    python main.py create")
+        print("    python main.py wallet")
         print("    python main.py spend --privatekey <privatekey> --amount <amount> --destination <destination> --walletname <walletname>")
         print("    python your_script.py spend -p <privatekey> -a <amount> -d <destination> -n <walletname>")
         return
     
     mode = sys.argv[1]
+    print(f'Mode: %s' % mode)
 
-    if mode == 'create':
+    if mode == 'wallet':
         WalletCreation()
         return
     elif mode == 'spend':
         import argparse
-        parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser(
+            usage="""
+                python main.py spend --privatekey <privatekey> --amount <amount>
+                                     --destination <destination> --walletname <walletname>
+                python your_script.py spend -p <privatekey> -a <amount> -d <destination> -n <walletname>
+            """,
+            description="Send an amount of tBTC to destination address using your wallet"
+        )
 
-        parser.add_argument("-p", "--privatekey", help="Private key")
-        parser.add_argument("-a", "--amount", help="Amount to send")
-        parser.add_argument("-d", "--destination", help="Destination address")
-        parser.add_argument("-n", "--walletname", help="Wallet name")
+        parser.add_argument("-p", "--privatekey", help="Private key", required=True)
+        parser.add_argument("-a", "--amount", help="Amount to send", required=True)
+        parser.add_argument("-d", "--destination", help="Destination address", required=True)
+        parser.add_argument("-n", "--walletname", help="Wallet name", required=True)
 
         args = parser.parse_args()
 
@@ -31,22 +39,8 @@ def main():
         destination_address = args.destination
         wallet_name = args.walletname
 
-        if private_key is None:
-            private_key = input("Enter private key: ")
-        
-        if amount is None:
-            amount = float(input("Amount to send: "))
-
-        if destination_address is None:
-            destination_address = input("Destination address: ")
-
-        if wallet_name is None:
-            wallet_name = input("Wallet name: ")
-
         wallet = OpenOrCreateWalletWithPrivKey(private_key=private_key, wallet_name=wallet_name)
         spendLockFunds(wallet=wallet, amount=amount, destination_address=destination_address)
     
 if __name__ == "__main__":
     main()
-
-    
